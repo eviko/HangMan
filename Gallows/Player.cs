e4c7017d78490227499eb0937e10c98323b6be6b
Evi <evdokiakogia@gmail.com>
2017-05-12 02:16:03 +0300

    Add files via upload

---
+++ Gallows/Player.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+    class Player
+    {
+        public string name { get; set; }
+        public string givenWord="";
+        public char givenChar { get; set; }
+        List<string> wordToFind = new List<string>();
+        List<string> LettersTried = new List<string>();
+        int playerLives = 5;
+        int remainingLetters = 0;
+
+
+        public Player()
+        {
+            // this.name = name;
+
+        }
+
+        public void DisplayTheWordToFind()
+
+        {
+
+            for (int i = 0; i < givenWord.Length; i++)
+            {
+                if (i == 0 || i == givenWord.Length - 1)
+
+                    wordToFind.Add(givenWord[i].ToString());
+                else
+                    wordToFind.Add("-");
+            }
+            foreach (string s in wordToFind)
+                Console.Write(s);
+
+        }
+
+        public void ChecktheLetter()
+        {
+
+            int countLetter = 0;
+            int count = 0;
+            
+
+            while (givenWord.Length != 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Give a letter");
+                string letter = Console.ReadLine().ToUpper();
+                givenChar = Convert.ToChar(letter);
+
+                if (givenWord.Contains(letter))
+                    for (int i = 0; i < givenWord.Length; i++)
+                    {
+                        if (givenWord[i] == givenChar)
+                        {
+                            wordToFind[i] = letter;
+                            countLetter++;
+                            count++;
+                        }
+
+                    }
+                else
+                {
+                    playerLives--;
+                    Console.WriteLine($"The letter {givenChar}  doesn't exist.Give another.The player {name} has {playerLives} lives.");                   
+                    LettersTried.Add(letter);
+                    foreach (string l in LettersTried)
+                        Console.Write(l);
+                    WinState();
+                }
+
+
+
+                if (countLetter != 0)
+                {
+                    remainingLetters = givenWord.Length-2 - count;
+                    WinState();
+                    Console.WriteLine($"The letter {givenChar} was found {countLetter} times.You have to find {remainingLetters} letters");
+                    foreach (string s in wordToFind)
+                        Console.Write(s);
+                    countLetter = 0;
+                }
+                else
+                    WinState();
+
+            }
+
+
+
+        }
+        public void WinState()
+        {
+            if (playerLives == 0)
+                Console.WriteLine($"You lost.");
+            else if (remainingLetters == 0)
+            {
+                Console.WriteLine($"You found the word {givenWord} ");
+            }
+        }
+
+
+    }
+}
---
+++ Gallows/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Console.WriteLine("Give the name of player1");
+            player1.name = Console.ReadLine();
+            Console.WriteLine("Give the name of player2");
+            player2.name = Console.ReadLine();
+
+
+            int firstPlayer = 0;
+
+            do
+            {
+                Console.WriteLine("Who plays first.For player 1 press 1,for player 2 press 2");
+                while (!int.TryParse(Console.ReadLine(), out firstPlayer))
+                {
+                    Console.WriteLine("Please give a number");
+                }
+            } while (firstPlayer != 1 && firstPlayer != 2);
+
+            if (firstPlayer == 1)
+            {
+                Console.WriteLine($"{player2.name} give the word");
+                player2.givenWord = Console.ReadLine().ToUpper();
+                Console.Clear();
+                player2.DisplayTheWordToFind();
+                player2.ChecktheLetter();
+
+            }
+            else
+            {
+                Console.WriteLine($"{player1.name} give the word");
+                player1.givenWord = Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
